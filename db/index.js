ee5c8b1e442fb548d4350139f1a56348eb886c81
christiangerbig <chr_gerbig@web.de>
2021-10-22 22:43:17 +0200

    no server access 1.5

--- db/index.js
+++ db/index.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+
+
+// import mongoose from "mongoose";
+
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
 
@@ -10,30 +14,10 @@ mongoose
     useCreateIndex: true,
   })
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-
-
-// import mongoose from "mongoose";
-
-// const MONGO_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
-
-// mongoose
-//   .connect(MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   })
-//   .then((x) => {
-//     console.log(
-//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to mongo: ", err);
-//   });
