4a8d7a4dfb1ccc53be8ecff4222ab5a5530974e4
christiangerbig <chr_gerbig@web.de>
2021-10-22 22:46:18 +0200

    no server access 1.6

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import { connect } from "mongoose";
 
 
 // import mongoose from "mongoose";
@@ -6,8 +6,7 @@ const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
 
-mongoose
-  .connect(MONGO_URI, {
+connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
