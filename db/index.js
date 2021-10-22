717d97f00ab3738cfe8fe4eb5bbebe5f0e0e010d
christiangerbig <chr_gerbig@web.de>
2021-10-22 22:39:46 +0200

    no server access 1.4

--- db/index.js
+++ db/index.js
@@ -1,22 +1,4 @@
-// const mongoose = require("mongoose");
-// const MONGO_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
-
-// mongoose
-//   .connect(MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   })
-//   .then((x) =>
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   )
-//   .catch((err) => console.error("Error connecting to mongo: ", err));
-
-
-import mongoose from "mongoose";
-
+const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
 
@@ -28,10 +10,30 @@ mongoose
     useCreateIndex: true,
   })
   .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
+
+
+// import mongoose from "mongoose";
+
+// const MONGO_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost/creative-gallery";
+
+// mongoose
+//   .connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   })
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo: ", err);
+//   });
